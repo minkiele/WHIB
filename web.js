839de9604127581ac90049cb754db6648e99aa27
Michele Montagner <michele@cowrider.local>
2013-12-09 23:49:16 +0100

    Static routing, that's nice\!

--- web.js
+++ web.js
@@ -1,15 +1,7 @@
 var express = require('express');
-var fs = require('fs');
-
-var indexStatic = fs.readFile('index.html', {encoding: 'utf-8'}, function(err, data){
-  var app = express();
-  app.get('/', function(req, res){
-    res.send(data);
-  });
-  var port = process.env.PORT || 5000;
-  app.listen(port, function(){
-    console.log('Listening on ' + port);
-  });
+var app = express();
+app.use(express.static(__dirname));
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+  console.log('Listening on ' + port);
 });
-
-  
