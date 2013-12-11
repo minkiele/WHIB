210e3652d833051e6953e7a3d68513ff5895bf81
Michele Montagner <michele@cowrider.local>
2013-12-12 00:30:54 +0100

    Added GMAPS_API support to heroku

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Startup
 Clone the repository, having installed *node* with *npm*, *grunt-cli* and *bower*.
 Next run *npm install* and *bower install* to install required *grunt* tasks and javascript assets.
 You'll want to run *grunt watch* command to compile every .coffee and .less file inside the project.
+
+Remember to call `heroku config:set GMAPS_KEY=GMAPS_API_KEY`
--- web.js
+++ web.js
@@ -6,6 +6,8 @@ var consolidate = require('consolidate');
 var app = express();
 
 app.engine('html', consolidate.hogan);
+app.set('view engine', 'html');
+app.set('views', '.');
 
 /*
  * Static routes and serve index.html
@@ -14,11 +16,15 @@ app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
 app.get('/', function(req,res){
-	res.sendfile('index.html');
+	res.render('index.html', {
+		GMAPS_KEY: process.env.GMAPS_KEY
+	});
 });
 
 //Port selector for Heroku (Env)/ Foreman (5000)
 var port = process.env.PORT || 5000;
+
+console.log(process.env.GMAPS_KEY);
 
 //Silence, the audience is listening
 app.listen(port, function(){
