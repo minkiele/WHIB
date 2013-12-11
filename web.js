834444a396e0a6efb00101a38ffc2cfc9a160f82
Michele Montagner <michele@cowrider.local>
2013-12-12 00:14:11 +0100

    Commit before pull

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var consolidate = require('consolidate');
 var app = express();
+app.engine('html', consolidate.hogan);
 app.use(express.static(__dirname));
 var port = process.env.PORT || 5000;
 app.listen(port, function(){
