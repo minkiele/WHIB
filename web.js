c2f37c96e6cf9079c0487c7576a13b13616ce4eb
Michele Montagner <michele.montagner@tsw.it>
2013-12-11 14:49:45 +0100

    Too much PHP will kill me

--- web.js
+++ web.js
@@ -7,9 +7,9 @@ var app = express();
 /*
  * Static routes and serve index.html
  */
-app.use('/bower_components', express.static(__dirname . '/bower_components'));
-app.use('/js', express.static(__dirname . '/js'));
-app.use('/css', express.static(__dirname . '/css'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
 app.get('/', function(req,res){
 	res.sendfile('index.html');
 });
