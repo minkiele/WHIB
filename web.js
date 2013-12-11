15a2836e167ce907c6c27d6a0da4c9fdb17c4415
Michele Montagner <michele.montagner@tsw.it>
2013-12-11 14:45:34 +0100

    Added static routes for files (do not server all the other files)

--- web.js
+++ web.js
@@ -1,7 +1,23 @@
+/*
+ * Start the app
+ */
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname));
+
+/*
+ * Static routes and serve index.html
+ */
+app.use('/bower_components', express.static(__dirname . '/bower_components'));
+app.use('/js', express.static(__dirname . '/js'));
+app.use('/css', express.static(__dirname . '/css'));
+app.get('/', function(req,res){
+	res.sendfile('index.html');
+});
+
+//Port selector for Heroku (Env)/ Foreman (5000)
 var port = process.env.PORT || 5000;
+
+//Silence, the audience is listening
 app.listen(port, function(){
   console.log('Listening on ' + port);
 });
